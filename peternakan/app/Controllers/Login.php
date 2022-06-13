6de67c84aff8830df432f1fd012bab62f4ae2bd4
suryamaris <smarispanensa@gmail.com>
2022-06-13 12:40:10 +0700

    Install CI 4

---
+++ peternakan/app/Controllers/Login.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Controllers;
+
+class Login extends BaseController
+{
+    public function index()
+    {
+        $data = ['title' => 'Halaman Login'];
+        return view('login', $data);
+    }
+}
